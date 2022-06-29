638c2bb904facadd784d0d513954e2a4b3aae963
Sai Anushka M <madyanamanushka@gmail.com>
2022-06-29 16:50:24 +0530

    Add files via upload

---
+++ BookStore-App/Backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:u4GgcrLVnwpDUHe3@cluster0.fm5vngf.mongodb.net/bookStore?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
